c18bbee1e86a7df665fd9ca2eb00f484a8b52b5c
vikram110703 <sainivks4321@gmail.com>
2023-07-14 18:06:04 +0530

    app.js updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-/* MODULES */
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
 import ScrollToTop from "react-scroll-to-top";
 
-/* STYLING */
 import "./App.css";
-
-/* COMPONENTS */
 
 import Navbar from "./components/Navbar";
 import Home from "./Routes/Home/Home";
 import About from "./components/About";
 import Dev from "./components/Dev";
-
-/* LOWER ORDER */
 import CustomLoader from "./components/CustomLoader";
 import Footer from "./components/Footer";
 
@@ -33,21 +27,30 @@ function App() {
         <CustomLoader />
       ) : (
         <div className="app__container">
-          <Router>
+          {/* <Router> */}
           <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
+            {/* <Route path="/" element={<Home />} /> */}
             <Route path="/about" element={<About />} />
-            <Route path="/dev" element={<Dev/>} />
-            <Route path="/*" Navigate to={"/"} /> 
+            <Route path="/dev" element={<Dev />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
           <Footer />
-          </Router>
           <ScrollToTop smooth color="black" />
+          {/* </Router> */}
         </div>
       )}
     </div>
   );
+}
+
+function NotFound() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/");
+  }, [navigate]);
+  return null;
 }
 
 export default App;
